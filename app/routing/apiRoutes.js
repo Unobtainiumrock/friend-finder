740cb6ab927b59ff7881854532cc0d13b1f7adf5
Unobtainiumrock <unobtainiumrock@gmail.com>
2018-06-05 22:28:19 -0700

    Testing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,42 +1,63 @@
+'use strict';
 
-const path = require('path');
-const users = require(path.join(__dirname, '../data/friends'));
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-class APIRoutes {
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-  attach(app) {
-    app.get('/api/friends', this.grabFriends);
-    app.post('/api/friends', this.handleSurvey.bind(this));
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var path = require('path');
+var users = require(path.join(__dirname, '../data/friends'));
+
+var APIRoutes = function () {
+  function APIRoutes() {
+    _classCallCheck(this, APIRoutes);
   }
 
-  grabFriends(req, res) {
-    const { body } = req;
-    res.status(200).json(users);
-  }
+  _createClass(APIRoutes, [{
+    key: 'attach',
+    value: function attach(app) {
+      app.get('/api/friends', this.grabFriends);
+      app.post('/api/friends', this.handleSurvey.bind(this));
+    }
+  }, {
+    key: 'grabFriends',
+    value: function grabFriends(req, res) {
+      var body = req.body;
 
-  handleSurvey(req, res) {
-    const { body } = req;
-    const bestFriend = this.findBestMatch(body);
-    users.push(body);
-    res.status(200).json(bestFriend);
-  }
+      res.status(200).json(users);
+    }
+  }, {
+    key: 'handleSurvey',
+    value: function handleSurvey(req, res) {
+      var body = req.body;
 
-  findBestMatch(body) {
-    const formAnswers = body.scores;
-    const differences = users.map(user => {
-      return user.scores.reduce((mem, val, idx) => {
-        return mem + Math.abs(formAnswers[idx] - val);
-      }, 0);
-    });
+      var bestFriend = this.findBestMatch(body);
+      users.push(body);
+      res.status(200).json(bestFriend);
+    }
+  }, {
+    key: 'findBestMatch',
+    value: function findBestMatch(body) {
+      var formAnswers = body.scores;
+      var differences = users.map(function (user) {
+        return user.scores.reduce(function (mem, val, idx) {
+          return mem + Math.abs(formAnswers[idx] - val);
+        }, 0);
+      });
 
-    const indexOfBestMatch = differences.indexOf(Math.min(...differences));
-    const bestMatch = users[indexOfBestMatch];
-    const { name, photo } = bestMatch;
-    
-    // users.push()
-    return { name, photo };
-  }
+      var indexOfBestMatch = differences.indexOf(Math.min.apply(Math, _toConsumableArray(differences)));
+      var bestMatch = users[indexOfBestMatch];
+      var name = bestMatch.name,
+          photo = bestMatch.photo;
 
-}
+      // users.push()
+
+      return { name: name, photo: photo };
+    }
+  }]);
+
+  return APIRoutes;
+}();
 
 module.exports = new APIRoutes();
--- server.js
+++ server.js
@@ -1,21 +1,24 @@
+'use strict';
+
 // Vendor
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 // const PORT = process.env.port || 3000;
-const path = require('path');
+var path = require('path');
 
 // Middleware
-const bodyParser = require('body-parser');
-app.use(express.static(path.join(__dirname,'/public')))
+var bodyParser = require('body-parser');
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
 // Routes 
-const APIRoutes = require(path.join(__dirname,'/app/routing/apiRoutes'));
+var APIRoutes = require(path.join(__dirname, '/app/routing/apiRoutes'));
 APIRoutes.attach(app);
 
-const HTMLRoutes = require(path.join(__dirname,'/app/routing/htmlRoutes'));
+var HTMLRoutes = require(path.join(__dirname, '/app/routing/htmlRoutes'));
 HTMLRoutes.attach(app);
 
-app.listen(process.env.port || 3000,() => console.log(`Listening on port: ${process.env.port || 3000}`));
+app.listen(process.env.port || 3000, function () {
+  return console.log('Listening on port: ' + (process.env.port || 3000));
+});
