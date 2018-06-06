4bc763e3c28251f1a4b59f8090e1ebe356fb0f2c
Unobtainiumrock <unobtainiumrock@gmail.com>
2018-06-05 22:30:40 -0700

    Found the bug

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,56 +1,36 @@
-'use strict';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+const path = require('path');
+const data = require(path.join(__dirname,'../data/friends'));
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var path = require('path');
-var data = require(path.join(__dirname, '../data/friends'));
-
-var HTMLRoutes = function () {
-  function HTMLRoutes() {
-    _classCallCheck(this, HTMLRoutes);
+class HTMLRoutes {
+  
+  /**
+   * attaches our routes to the express server
+   * @param  {Object} app is the instance of our express server
+   */
+  attach(app) {
+    app.get('/view',this.nonSense);
+    app.get('/survey',this.sendSurvey);
+    app.get('/',this.home);
   }
 
-  _createClass(HTMLRoutes, [{
-    key: 'attach',
+  sendSurvey(req,res) {
+    let { body } = req;
+    console.log(body);
+    res.sendFile(path.join(__dirname,'../public/survey.html'));
+  }
 
+  home(req,res) {
+    let { body } = req;
+    res.sendFile(path.join(__dirname,'../public/home.html'));
+  }
 
-    /**
-     * attaches our routes to the express server
-     * @param  {Object} app is the instance of our express server
-     */
-    value: function attach(app) {
-      app.get('/view', this.nonSense);
-      app.get('/survey', this.sendSurvey);
-      app.get('/', this.home);
-    }
-  }, {
-    key: 'sendSurvey',
-    value: function sendSurvey(req, res) {
-      var body = req.body;
+  nonSense(req,res) {
+    let { body } = req;
+    console.log(body);
+    res.sendFile(path.join(__dirname,'../public/potential-friends.html'));
+  }
 
-      console.log(body);
-      res.sendFile(path.join(__dirname, '../public/survey.html'));
-    }
-  }, {
-    key: 'home',
-    value: function home(req, res) {
-      var body = req.body;
-
-      res.sendFile(path.join(__dirname, '../public/home.html'));
-    }
-  }, {
-    key: 'nonSense',
-    value: function nonSense(req, res) {
-      var body = req.body;
-
-      console.log(body);
-      res.sendFile(path.join(__dirname, '../public/potential-friends.html'));
-    }
-  }]);
-
-  return HTMLRoutes;
-}();
+}
 
 module.exports = new HTMLRoutes();
--- server.js
+++ server.js
@@ -1,24 +1,21 @@
-'use strict';
-
 // Vendor
-var express = require('express');
-var app = express();
-// const PORT = process.env.port || 3000;
-var path = require('path');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
 
 // Middleware
-var bodyParser = require('body-parser');
-app.use(express.static(path.join(__dirname, '/public')));
+const bodyParser = require('body-parser');
+app.use(express.static(path.join(__dirname,'/public')))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
 // Routes 
-var APIRoutes = require(path.join(__dirname, '/app/routing/apiRoutes'));
+const APIRoutes = require(path.join(__dirname,'/app/routing/apiRoutes'));
 APIRoutes.attach(app);
 
-var HTMLRoutes = require(path.join(__dirname, '/app/routing/htmlRoutes'));
+const HTMLRoutes = require(path.join(__dirname,'/app/routing/htmlRoutes'));
 HTMLRoutes.attach(app);
 
-app.listen(process.env.port || 3000, function () {
-  return console.log('Listening on port: ' + (process.env.port || 3000));
-});
+app.listen(PORT,() => console.log(`Listening on port: ${PORT}`));
