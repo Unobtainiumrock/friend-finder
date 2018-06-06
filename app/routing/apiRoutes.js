7d50c50b22f394997399beb952123f4a5d4a4dc5
Unobtainiumrock <unobtainiumrock@gmail.com>
2018-06-05 22:45:36 -0700

    Add parsInt to make all the form data become numbers

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,13 +16,16 @@ class APIRoutes {
 
   handleSurvey(req, res) {
     const { body } = req;
-    const bestFriend = this.findBestMatch(body);
+    let { scores } = body;
+    scores = scores.map(score => parseInt(score));
+    body['scores'] = scores;
+    const bestFriend = this.findBestMatch(scores);
     users.push(body);
     res.status(200).json(bestFriend);
   }
 
-  findBestMatch(body) {
-    const formAnswers = body.scores;
+  findBestMatch(scores) {
+    const formAnswers = scores;
     const differences = users.map(user => {
       return user.scores.reduce((mem, val, idx) => {
         return mem + Math.abs(formAnswers[idx] - val);
@@ -32,8 +35,6 @@ class APIRoutes {
     const indexOfBestMatch = differences.indexOf(Math.min(...differences));
     const bestMatch = users[indexOfBestMatch];
     const { name, photo } = bestMatch;
-    
-    // users.push()
     return { name, photo };
   }
 
